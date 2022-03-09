d41fb7a8336962d9e0339254cd1e822d42fc2d27
simba155kor <66295630+simba155kor@users.noreply.github.com>
2022-03-09 19:31:43 +0900

    Create kruscal.cpp

---
+++ algo/kruscal.cpp
@@ -0,0 +1,29 @@
+
+
+//다익스트라, 크루스칼은 시작할때 항상 그래프 길이 초기화 하고 시작.
+
+for(int a=1; a<=n; a++)
+{
+    for(int b=1; b<=n; b++)
+    {
+        if(a==b) continue;
+        board[a][b] = MAX;
+    }
+}
+
+
+
+
+
+for(int k=1; k<=n; k++)
+{
+    for(int a=1; a<=n; a++)
+    {
+          for(int b=1; b<=n; b++)
+          {
+              //경유해서 가는 것이 더 최소라면 바꿔준다.
+              if( board[a][b] > board[a][k] + board[k][b]) board[a][b] = board[a][k] + board[k][b];          
+          }
+    }
+}
+
