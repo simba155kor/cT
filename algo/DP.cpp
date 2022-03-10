f52ec15efa1d54db5e9a8ad16659980b17e4a2d7
simba155kor <66295630+simba155kor@users.noreply.github.com>
2022-03-10 12:01:32 +0900

    Update DP.cpp

--- algo/DP.cpp
+++ algo/DP.cpp
@@ -5,3 +5,28 @@
 //Top-down  -> 재귀 
 
 //DFS + dp 혹은 DFS + 메모이제이션 -> 재귀, top-down
+
+
+
+//dfs + dp
+//그냥 dp로 해주어야 하는데 거기서 또 만들어 져야하면
+//거기를 dfs(a,b)로 바꿔주기만 하면 된다.
+
+int dfs(int y, int x)
+{
+    //기저조건
+    if(y==0 && x==0) return 0;
+    
+    //이미 방문해서 만들었다면 그걸 바로 리턴.
+    if(dp[y][x] != -1) return dp[y][x];
+    
+    //방문한 적이 없다는 거야.
+    //dp[now]를 만들어서 리턴해야지.
+    
+    dp[y][x] = min(dfs(y-1, x), dfs(y, x-1));
+    //또는
+    dp[y][x] += dfs(y-1, x);
+    dp[y][x] += dfs(y, x-1);
+  
+    return dp[now];
+}
